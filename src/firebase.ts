69a3822315c1ea71edd4288895b7dbae5526f197
Kevin <haoranw1402@gmail.com>
2025-06-22 13:13:09 -0700

    fix

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA_vYsEwGPdHyZdSgsqbJmqM1ExJSoxcY4",
-  authDomain: "answerai-748a3.firebaseapp.com",
-  projectId: "answerai-748a3",
-  storageBucket: "answerai-748a3.firebasestorage.app",
-  messagingSenderId: "1032129787336",
-  appId: "1:1032129787336:web:c8c71ca624c1f0f049fcfd",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
